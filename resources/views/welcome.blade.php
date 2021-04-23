94fcc48b3aca0648b24e974695652aa1648f6517
Soikat Rahman <hello.dev.sr@gmail.com>
2021-04-23 15:44:36 +0600

    build prod v2.2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,14 @@
     <title>GreenLib</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    <!-- -- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> ---->
     <link href="/css/app.css" rel="stylesheet">
 
 </head>
 
 <body>
     <div id="app"></div>
-    {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+    <!--- <script src="{{ asset('js/app.js') }}"></script>  -->
     <script src="js/app.js"></script>
 </body>
 
